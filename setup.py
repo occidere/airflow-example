d561c9e78e60ce901cc1bfd9a71ee9d1d57da9bc
occidere <occidere@naver.com>
2020-01-14 22:46:32 +0900

    Project Init

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='airflow-example',
+        version='1.0',
+        author='occidere',
+        author_email='occidere@naver.com',
+        url='https://github.com/occidere/airflow-example',
+        description='No Description',
+        license='Apache 2.0',
+        install_requires=[
+            'airflow'
+        ],
+        packages=find_packages(exclude=['venv', 'doc', 'test']),
+        python_requires='>=3.5',
+        package_data={
+            'resources': ['resources/*']
+        },
+        zip_safe=False,
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+        ]
+)
